5229dbb2d1d3af1f814cacc3e36d94a981972ef7
beingash22 <ashrockzap@gmail.com>
2018-01-10 22:45:35 +0530

    Create numcheck.c

---
+++ numcheck.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+int n,num,res;
+printf ("enter a number");
+scanf ("%d",&n);
+
+num=n;
+
+res=num+n;
+
+if (res==0)
+printf ("negative number");
+else
+printf ("positive number");
+}
