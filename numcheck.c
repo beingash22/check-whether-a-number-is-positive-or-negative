c23479f4d2818bcccacbc057e4a5579814474ddc
beingash22 <ashrockzap@gmail.com>
2018-01-10 22:52:27 +0530

    Update numcheck.c

--- numcheck.c
+++ numcheck.c
@@ -2,15 +2,15 @@
 
 void main()
 {
-int n,num,res;
+double n,num,res;
 printf ("enter a number");
-scanf ("%d",&n);
-
+scanf ("%lf",&n);
+ 
 num=n;
 
-res=num+n;
+res=num-n;
 
-if (res==0)
+if (res==0.0)
 printf ("negative number");
 else
 printf ("positive number");
